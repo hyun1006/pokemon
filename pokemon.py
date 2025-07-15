f9640e2d85d44a67119c5d60745454c755c3795e
hyun1006 <doitndidit@gmail.com>
2025-07-15 15:17:41 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -12,6 +12,9 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path="/usr/bin/chromedriver")
+
 options = Options()
 options.add_argument("--headless")  # 창 없음
 options.add_argument("--no-sandbox")
